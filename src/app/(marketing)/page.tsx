37c3d51fc4e96babf2fe46604d30ecbb85e42f5c
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-01-18 14:48:10 -0500

    Started working on homepage, added logo component, added markup and styling to homepage

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,34 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+
 export default function HomePage() {
-	return <main>Home Page</main>;
+	return (
+		<main className="flex flex-col xl:flex-row items-center justify-center gap-10 bg-[#5DC9A8] min-h-screen">
+			<Image
+				src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+				alt="Preview of Petsoft"
+				width={519}
+				height={472}
+			/>
+
+			<div>
+				<Logo />
+
+				<h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+					Manage your <span className="font-extrabold">pet daycare</span> with
+					ease
+				</h1>
+
+				<p className="text-2xl font-medium max-w-[600px]">
+					Use PetSoft to easily keep track of pets under your care. Get lifetime
+					access for $299.
+				</p>
+
+				<div className="mt-10">
+					<button>Sign up</button>
+					<button>Log in</button>
+				</div>
+			</div>
+		</main>
+	);
 }
