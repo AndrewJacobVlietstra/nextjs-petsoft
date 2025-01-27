69d3bd42258f40ea53946f13b3b28d22a4306ee7
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-01-27 16:51:37 -0500

    Transitioning pet-form to use react-hook-form

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { actionType } from "@/lib/types";
+import { actionType, PetFormData } from "@/lib/types";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { usePetContext } from "@/lib/hooks";
-import PetFormBtn from "./pet-form-btn";
 import { PLACEHOLDER_IMG } from "@/lib/constants";
+import { useForm } from "react-hook-form";
+import PetFormBtn from "./pet-form-btn";
 
 export type remainingActions = Exclude<actionType, "checkout">;
 
@@ -17,10 +18,20 @@ type PetFormProps = {
 
 export default function PetForm({ actionType, closeDialog }: PetFormProps) {
 	const { selectedPet, handleAddPet, handleEditPet } = usePetContext();
+	const {
+		register,
+		trigger,
+		formState: { errors },
+	} = useForm<PetFormData>();
 
 	return (
 		<form
 			action={async (formData) => {
+				const result = await trigger();
+				if (!result) return;
+
+				closeDialog();
+
 				const petData = {
 					name: formData.get("name") as string,
 					ownerName: formData.get("ownerName") as string,
@@ -36,61 +47,58 @@ export default function PetForm({ actionType, closeDialog }: PetFormProps) {
 					await handleEditPet(selectedPet!.id, petData);
 				}
 			}}
-			onSubmit={() => closeDialog()}
 			className="flex flex-col gap-y-4"
 		>
 			<div className="space-y-1">
 				<Label htmlFor="name">Name</Label>
 				<Input
 					id="name"
-					name="name"
-					type="text"
-					required
-					defaultValue={actionType === "edit" ? selectedPet?.name : ""}
+					{...register("name", {
+						required: "Name is required.",
+						maxLength: {
+							value: 30,
+							message: "Name has a maximum of 30 characters long.",
+						},
+					})}
 				/>
+				{errors.name && <p className="text-red-500">{errors.name.message}</p>}
 			</div>
 
 			<div className="space-y-1">
 				<Label htmlFor="ownerName">Owner Name</Label>
 				<Input
 					id="ownerName"
-					name="ownerName"
-					type="text"
-					required
-					defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
+					{...register("ownerName", {
+						required: "Owner Name is required.",
+						maxLength: {
+							value: 30,
+							message: "Owner Name has a maximum of 30 characters long.",
+						},
+					})}
 				/>
+				{errors.ownerName && (
+					<p className="text-red-500">{errors.ownerName.message}</p>
+				)}
 			</div>
 
 			<div className="space-y-1">
 				<Label htmlFor="age">Age</Label>
-				<Input
-					id="age"
-					name="age"
-					type="number"
-					required
-					defaultValue={actionType === "edit" ? selectedPet?.age : ""}
-				/>
+				<Input id="age" {...register("age")} />
+				{errors.age && <p className="text-red-500">{errors.age.message}</p>}
 			</div>
 
 			<div className="space-y-1">
 				<Label htmlFor="imageUrl">Image URL</Label>
-				<Input
-					id="imageUrl"
-					name="imageUrl"
-					type="text"
-					defaultValue={actionType === "edit" ? selectedPet?.imageUrl : ""}
-				/>
+				<Input id="imageUrl" {...register("imageUrl")} />
+				{errors.imageUrl && (
+					<p className="text-red-500">{errors.imageUrl.message}</p>
+				)}
 			</div>
 
 			<div className="space-y-1">
 				<Label htmlFor="notes">Notes</Label>
-				<Textarea
-					id="notes"
-					name="notes"
-					rows={3}
-					required
-					defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
-				/>
+				<Textarea id="notes" {...register("notes")} rows={3} />
+				{errors.notes && <p className="text-red-500">{errors.notes.message}</p>}
 			</div>
 
 			<PetFormBtn actionType={actionType} />
