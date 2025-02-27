e8a4f1c8524a56badb8d44ea4864816eaf0643ad
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-02-26 23:47:25 -0500

    Added logic to stripe api route handler to fulfill checkout order and change user access to true in prisma db. Using ngrok as webhook proxy between stripe and server.

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,4 +1,20 @@
+import { prisma } from "@/lib/prisma";
+
 export async function POST(request: Request) {
-	const body = await request.text();
-	console.log(body);
+	const data = await request.json();
+
+	// verify webhook came from stripe
+
+	// fulfill order
+	await prisma.user.update({
+		where: {
+			email: data.data.object.customer_email,
+		},
+		data: {
+			hasAccess: true,
+		},
+	});
+
+	// return 200 ok status
+	return Response.json(null, { status: 200 });
 }
